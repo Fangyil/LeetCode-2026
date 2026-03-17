e007b9450ffaf67c2f80e17518bc6b75d8c0f54a
Fangyil <102721215+Fangyil@users.noreply.github.com>
2026-03-18 00:12:09 +0800

    Update link storage format in update_problems.py

--- scripts/update_problems.py
+++ scripts/update_problems.py
@@ -71,7 +71,7 @@ for folder in FOLDERS:
         link = f"https://leetcode.com/problems/{slug}/"
 
         # 存進 dict
-        problems_dict[pid].append((title_fmt, difficulty, method, link, method, f))
+        problems_dict[pid].append((title_fmt, difficulty, method, link, folder, f))
 
 # -----------------------------
 # 🔹 生成 README 區塊
